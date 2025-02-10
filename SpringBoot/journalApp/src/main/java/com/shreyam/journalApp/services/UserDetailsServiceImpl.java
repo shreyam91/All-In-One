76432bfb73b941999bb750b37fb1d87189f04d43
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-10 21:41:55 +0530

    try to solve error...

--- SpringBoot/journalApp/src/main/java/com/shreyam/journalApp/services/UserDetailsServiceImpl.java
+++ SpringBoot/journalApp/src/main/java/com/shreyam/journalApp/services/UserDetailsServiceImpl.java
@@ -1,32 +1,30 @@
 package com.shreyam.journalApp.services;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.shreyam.journalApp.entity.User;
 import com.shreyam.journalApp.repository.UserRepository;
 
-@Component
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
 
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUserName(username);
-        if(user != null){
-          UserDetails userDetails =   org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUserName())
-                .password(user.getPassword())
-                .roles(user.getRoles().toArray(new String[0]))
-                .build();
-            return userDetails;
+        if (user != null) {
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getUserName())
+                    .password(user.getPassword())
+                    .roles(user.getRoles().toArray(new String[0]))
+                    .build();
         }
-        return null;
+        throw new UsernameNotFoundException("User not found with username: " + username);
     }
-
 }
--- SpringBoot/journalApp/src/main/java/com/shreyam/journalApp/services/UserService.java
+++ SpringBoot/journalApp/src/main/java/com/shreyam/journalApp/services/UserService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.shreyam.journalApp.entity.User;
@@ -15,7 +17,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public void saveEntry(User user){
+        userRepository.save(user);
+    }
+    
+    public void saveNewUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 

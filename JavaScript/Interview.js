78a59d04262e8549715a42b7f5b151a3d6f2bab0
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-10 11:45:15 +0530

    Interview question JS...

---
+++ JavaScript/Interview.js
@@ -0,0 +1,42 @@
+// let a =10;
+// {
+//     var a =10;
+//     console.log(a); // Can't declare (a) again.
+// }
+// console.log(a);
+
+// left -> right
+
+var a =10;
+{
+    let a =20;
+    console.log(a); // 20
+}
+console.log(a); // 10
+
+console.log("1"-1); // 0
+console.log("javascript" +'10');  // javascript10
+console.log(1-"sid"); // NaN
+console.log("2"+"1"+1);    // 211 
+console.log(2+1+"1"); //31
+console.log("2"+"1"-1); //20
+
+console.log("sid"%1); // NaN
+console.log("2"-"1"); // 1
+
+console.log(NaN==NaN); //false
+console.log(NaN === NaN); //false
+console.log(NaN.isNaN); //undefined
+
+let c = NaN;
+console.log(isNaN(c)); // true
+let b = NaN;
+console.log(isNaN(c) && isNaN(b));
+
+let n =10;
+// console.log(++n); // 11
+// console.log(n++); // 10
+// console.log(n); //11
+console.log(++n+n++);
+
+// if there is one string and one number and there is any operator except then + the answer is always NaN.

d8355cd6362989044af82d16c521b1a07ff5c2c5
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-17 22:24:47 +0530

    working on Pagination machine coding question..

--- ReactInterview/reactCodingQuestion/src/App.jsx
+++ ReactInterview/reactCodingQuestion/src/App.jsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
-function App() {
+const ProductCard = ({image,title}) =>{
+  return <div className='product-card'> 
+    <img src={image} alt={title} className='product-img'/>
+    <span className='product-title'>{title}</span>
+  </div>
+}
 
-  return (
+const PAGE_SIZE = 5;
+
+function App() {
+const [products, setProducts] = useState([]);
+
+const fetchData = async () =>{
+  const data = await fetch("https://dummyjson.com/products");
+  const json = await data.json();
+  setProducts(json.products);
+}
+
+useEffect(() =>{
+  fetchData();
+},[])
+
+const totalProducts = products.length;
+const noOfPages = Math.ceil(totalProducts/PAGE_SIZE);
+
+  return !products.length ? <h2> No Products found...</h2> :(
     <div>
-      <h2>Hello</h2>
+      <h2>Pagination</h2>
+      <div>
+        {[...Array[10].keys()]}
+      </div>
+      <div className='products'>
+      {
+        products.map((p) =>( <ProductCard key={p.id} image={p.thumbnail} title={p.title}/> )
+      )}
+      </div>
     </div>
   )
 }

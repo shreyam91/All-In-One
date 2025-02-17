3115789f8c2f981459502348da3a29190f3f0109
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-17 22:38:28 +0530

    JS interview Question...

---
+++ JavaScript/Feb17.js
@@ -0,0 +1,9 @@
+// const obj = {name:"JS"}
+// const arr = ["name"]
+// obj[arr] = "React"
+// console.log(obj); //{name: 'React'}
+
+const obj = {name:"JS"}
+const arr = ["name","age"]
+obj[arr] = "React"
+console.log(obj); //{ name: 'JS', 'name,age': 'React' }

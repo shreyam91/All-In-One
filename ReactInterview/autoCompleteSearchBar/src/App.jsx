36ebb4b521ea678b1c178e26652186da4163f958
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-27 12:19:57 +0530

    Project completed... ✅

--- ReactInterview/autoCompleteSearchBar/src/App.jsx
+++ ReactInterview/autoCompleteSearchBar/src/App.jsx
@@ -1,10 +1,52 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const [result, setResult] = useState("")
+  const [input, setInput] = useState([])
+  const [showResult, setShowResult] = useState(false);
+  const [cache, setCache] = useState({});
+
+  const fetchData = async() =>{
+    if(cache[input]){
+      setResult(cache[input]);
+      return;
+    }
+    
+    const data = await fetch ('https://dummyjson.com/recipes/search?q=' + input);
+    const json = await data.json();
+    setResult(json?.recipes)
+    setCache(prev => ({...prev,[input]:json?.recipes}));
+  }
+
+  useEffect(() =>{
+    const timer = setTimeout(fetchData, 300);
+
+    return () =>{
+      clearTimeout(timer);
+    }
+  },[input])
+
   return (
    <>
     <h2>Auto Complete Search Bar</h2>
+    <div>
+      <input 
+      type="text" 
+      className='search-input' 
+      value={input} 
+      onChange={(e) => setInput(e.target.value)}
+      onFocus={() => setShowResult(true)}
+      onBlur={() => setShowResult(false)}
+      />
+    </div>
+
+    {showResult && (
+      <div className='result-container'>
+      {result.map((r) => <span key={r.id} className='result'>{r.name}</span>)}
+    </div>
+    )}
    </>
   )
 }

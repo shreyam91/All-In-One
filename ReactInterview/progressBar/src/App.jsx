39dd017583d3943de1c3187e451a3b449c441607
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-03-07 16:18:22 +0530

    basic progress bar completed..

--- ReactInterview/progressBar/src/App.jsx
+++ ReactInterview/progressBar/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+const ProgressBar = ({progress}) => {
+  return ( 
+  <div className='outer'>
+
+    <div className='inner' 
+    style={{width: `${progress}%`, 
+    color: progress < 5 ? 'black' : 'white'}}
+    role="progressbar"
+    aria-valuenow={progress}
+    aria-valuemax='100'
+    aria-valuemin='0'
+    >
+    {progress}% 
+    </div>
+  </div>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const bars = [0,5,10,30,50,70,90,100];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className='name'> Progress Bar</h1>
+
+      {bars.map((value)=> (
+        <ProgressBar key={value} progress={value} className='bar'/>
+        ))}
+    </div>
   )
 }
 

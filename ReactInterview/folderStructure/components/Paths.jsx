9019461917c5fdeabb25018f1d3c5eb1d21ccddf
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-03-22 20:10:43 +0530

    working....

---
+++ ReactInterview/folderStructure/components/Paths.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Paths = () => {
+  return (
+    <>
+    {/* <section class="mt-6"> */}
+  <div class="container">
+    
+    {/* <!-- Left part: Heading, Content, and 2 Links in a Row --> */}
+    <div class="left-part">
+      <h2 class="text-xl font-bold">Get Certified</h2>
+      <p class="mt-2">
+        Certification modules, taken during your onboarding and annually after that, provide you with an overview of the Medicare program, an understanding of Medicare insurance options and the rules and regulations that guide your marketing and sales activities.
+      </p>
+      <div class="links-container">
+        <a class="link" href="#">2025 Certifications Instructions
+          <i class="fas fa-external-link-alt ml-2"></i>
+        </a>
+        <a class="link" href="#">2025 Certifications Instructions
+          <i class="fas fa-external-link-alt ml-2"></i>
+        </a>
+        <a class="link" href="#">2025 Certifications Instructions
+          <i class="fas fa-external-link-alt ml-2"></i>
+        </a>
+        <a class="link" href="#">2025 Certifications Decision Tree
+          <i class="fas fa-external-link-alt ml-2"></i>
+        </a>
+      </div>
+    </div>
+
+    {/* <!-- Right part: Image type video --> */}
+    <div class="right-part">
+      <div class="video-thumbnail">
+        <img alt="Video thumbnail with a play button and text 'WHICH PATH SHOULD YOU TAKE?'" src="https://placehold.co/300x200"/>
+      </div>
+    </div>
+  </div>
+{/* </section> */}
+    </>
+  )
+}
+
+export default Paths
---
+++ ReactInterview/folderStructure/components/View.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const View = () => {
+  return (
+    <>
+    <div className='certification'>
+        <p className='paragraph'> <span className='heading'>View your Certification Status! </span> <br /> Learn about products you are certified to sell.</p>
+        <button className='button'>Certification status</button>
+    </div>
+    </>
+  )
+}
+
+export default View

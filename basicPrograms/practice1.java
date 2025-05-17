419a9f09e020c67d6a8518a6c6498ff161955740
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-05-17 14:56:53 +0530

    practice

---
+++ basicPrograms/practice1.java
@@ -0,0 +1,84 @@
+
+
+public class practice1 {
+    // public static int sumArray (int[] arr){
+    //     int n = arr.length;
+    //     int sum = 0;
+
+    //     for (int  i =0; i<n;i++){
+    //         sum += arr[i];
+    //     }
+
+    //     return sum;
+    // }
+
+    // static int largest(int[]arr){
+    //     int n = arr.length;
+    //     int largest = arr[0];
+
+    //     for(int i =0; i<n;i++){
+    //         if(arr[i] > largest){
+    //             largest = arr[i];
+    //         }
+    //     }
+
+    //     return largest;
+    // }
+
+        // static int missing(int[]arr){
+        //     int n = arr.length;
+        //     int total = n *(n+1)/2;
+        //     int sum = 0;
+
+        //     for(int num:arr){
+        //         sum += num;
+        //     }
+        //     return total-sum;
+        // }
+
+        // static int reverseDigit(int num){
+        //     int reversed = 0;
+
+        //     while(num != 0){
+        //         int digit = num %10;
+        //         reversed = reversed *10 + digit;
+        //         num /= 10;
+        //     }
+        //     return reversed;
+        // }
+
+        // static String reverseString(String s){
+        //     String reverse ="";
+        //     int length = s.length();
+
+        //     for(int i = length-1; i>=0; i--){
+        //         reverse += s.charAt(i);
+        //     }
+        //     return reverse;
+        // }
+
+        
+
+
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,4,5,6};
+        int n = 12345;
+        String s = "Hello";
+        // System.out.println(sumArray(arr));
+        // System.out.println(largest(arr));
+        // System.out.println(missing(arr));
+        // System.out.println(reverseDigit(n));
+        // System.out.println(reverseString(s));
+
+        // for(int i=0; i<5;i++){
+        //     for(int j=0;j<2;j++){
+        //         System.out.println("hello");  // 10 times
+        //     }
+        // }
+
+        
+    }
+}
+
+
